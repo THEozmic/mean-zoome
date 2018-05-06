b186e8b53cd51dab64fa0e38642ffd745532bbf0
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-05-06 06:57:29 +0100

    Update mongo url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret':'meansecure',
-  'database': 'mongodb://localhost/mean-temp'
+  'secret':'mhhdilnl94ks94peirnbaqqnhi',
+  'database': 'mongodb://heroku_nmbmrltd:mhhdilnl94ks94peirnbaqqnhi@ds129066.mlab.com:29066/heroku_nmbmrltd'
 };
